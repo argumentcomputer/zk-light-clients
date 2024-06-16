7b95560a382b0e8733b9ff204c391c959739bdea
Artem Storozhuk <storojs72@gmail.com>
2024-06-16 16:29:38 +0100

    feat: Add Plonk contract

---
+++ aptos/solidity/contracts/src/EpochChange.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.25;
+
+import {SP1Verifier as SP1PlonkVerifier} from "../src/plonk/SP1Verifier.sol";
+
+contract EpochChange is SP1PlonkVerifier {
+    bytes32 public epochChangeProgramVkey;
+
+    constructor(bytes32 _epochChangeProgramVkey) {
+        epochChangeProgramVkey = _epochChangeProgramVkey;
+    }
+
+    function verifyProof(bytes memory proof, bytes memory publicValues) public view {
+        this.verifyProof(epochChangeProgramVkey, publicValues, proof);
+    }
+}
