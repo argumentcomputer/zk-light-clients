3c8083f471edb189a152077898b688c3093f899b
Artem Storozhuk <storojs72@gmail.com>
2024-06-20 18:32:51 +0100

    feat: Solidity verification of light client proofs with Plonk (#28)
    * feat: Add scaffolding for Plonk verification in Solidity
    
    * feat: Add Plonk contract
    
    * tests: Add more negative Solidity testing
    
    * feat: Add Rust program for Solidity contract generating
    
    * chore: Update Cargo.lock
    
    * chore: Update Plonk contract according to 'e48c01ec' tag of Sphinx
    
    * chore: Formatting
    
    * feat: Introduce downloading from our AWS private bucket
    
    * feat: Update Foundry project to use sphinx-contracts dependency
    
    * chore: Rename SP1 -> Sphinx

--- aptos/light-client/src/epoch_change.rs
+++ aptos/light-client/src/epoch_change.rs
@@ -12,6 +12,38 @@ struct EpochChangeOutput {
     new_validator_verifier_hash: [u8; 32],
 }
 
+#[cfg(feature = "aptos")]
+pub fn setup_assets() -> (Vec<u8>, Vec<u8>, Vec<u8>) {
+    use aptos_lc_core::aptos_test_utils::wrapper::AptosWrapper;
+    use aptos_lc_core::crypto::hash::CryptoHash;
+
+    const NBR_VALIDATORS: usize = 130;
+    const AVERAGE_SIGNERS_NBR: usize = 95;
+
+    let mut aptos_wrapper = AptosWrapper::new(20000, NBR_VALIDATORS, AVERAGE_SIGNERS_NBR).unwrap();
+
+    let trusted_state = bcs::to_bytes(aptos_wrapper.trusted_state()).unwrap();
+    let validator_verifier_hash =
+        match aptos_lc_core::types::trusted_state::TrustedState::from_bytes(&trusted_state).unwrap()
+        {
+            aptos_lc_core::types::trusted_state::TrustedState::EpochState {
+                epoch_state, ..
+            } => epoch_state.verifier().hash().to_vec(),
+            _ => panic!("Expected epoch change for current trusted state"),
+        };
+    let trusted_state_version = *aptos_wrapper.current_version();
+
+    aptos_wrapper.generate_traffic().unwrap();
+
+    let state_proof = aptos_wrapper
+        .new_state_proof(trusted_state_version)
+        .unwrap();
+
+    let epoch_change_proof = bcs::to_bytes(state_proof.epoch_changes()).unwrap();
+
+    (trusted_state, epoch_change_proof, validator_verifier_hash)
+}
+
 pub fn generate_stdin(current_trusted_state: &[u8], epoch_change_proof: &[u8]) -> SphinxStdin {
     let mut stdin = SphinxStdin::new();
     stdin.write(&current_trusted_state);
@@ -57,6 +89,7 @@ fn prove_epoch_change(
 
 #[cfg(all(test, feature = "aptos"))]
 mod test {
+    use crate::epoch_change::setup_assets;
     use crate::error::LightClientError;
     use sphinx_sdk::utils::setup_logger;
     use sphinx_sdk::{ProverClient, SphinxStdin};
@@ -85,29 +118,13 @@ mod test {
 
     #[test]
     fn test_execute_epoch_change() {
-        use aptos_lc_core::aptos_test_utils::wrapper::AptosWrapper;
         use std::time::Instant;
 
-        const NBR_VALIDATORS: usize = 130;
-        const AVERAGE_SIGNERS_NBR: usize = 95;
-
-        let mut aptos_wrapper =
-            AptosWrapper::new(20000, NBR_VALIDATORS, AVERAGE_SIGNERS_NBR).unwrap();
-
-        let trusted_state = bcs::to_bytes(aptos_wrapper.trusted_state()).unwrap();
-        let trusted_state_version = *aptos_wrapper.current_version();
-
-        aptos_wrapper.generate_traffic().unwrap();
-
-        let state_proof = aptos_wrapper
-            .new_state_proof(trusted_state_version)
-            .unwrap();
-
-        let epoch_change_proof = &bcs::to_bytes(state_proof.epoch_changes()).unwrap();
+        let (trusted_state, epoch_change_proof, _) = setup_assets();
 
         println!("Starting execution of prove_epoch_change...");
         let start = Instant::now();
-        execute_epoch_change(&trusted_state, epoch_change_proof).unwrap();
+        execute_epoch_change(&trusted_state, &epoch_change_proof).unwrap();
         println!("Execution took {:?}", start.elapsed());
     }
 
@@ -115,39 +132,17 @@ mod test {
     #[ignore = "This test is too slow for CI"]
     fn test_prove_epoch_change() {
         use super::*;
-        use aptos_lc_core::aptos_test_utils::wrapper::AptosWrapper;
-        use aptos_lc_core::crypto::hash::CryptoHash;
-        use aptos_lc_core::types::trusted_state::TrustedState;
         use sphinx_sdk::ProverClient;
         use std::time::Instant;
 
-        const NBR_VALIDATORS: usize = 130;
-        const AVERAGE_SIGNERS_NBR: usize = 95;
-
-        let mut aptos_wrapper =
-            AptosWrapper::new(20000, NBR_VALIDATORS, AVERAGE_SIGNERS_NBR).unwrap();
-
-        let trusted_state = bcs::to_bytes(aptos_wrapper.trusted_state()).unwrap();
-        let validator_verifier_hash = match TrustedState::from_bytes(&trusted_state).unwrap() {
-            TrustedState::EpochState { epoch_state, .. } => epoch_state.verifier().hash().to_vec(),
-            _ => panic!("Expected epoch change for current trusted state"),
-        };
-        let trusted_state_version = *aptos_wrapper.current_version();
-
-        aptos_wrapper.generate_traffic().unwrap();
-
-        let state_proof = aptos_wrapper
-            .new_state_proof(trusted_state_version)
-            .unwrap();
-
-        let epoch_change_proof = &bcs::to_bytes(state_proof.epoch_changes()).unwrap();
+        let (trusted_state, epoch_change_proof, validator_verifier_hash) = setup_assets();
 
         let client = ProverClient::new();
 
         let start = Instant::now();
         println!("Starting generation of prove_epoch_change proof...");
         let (proof, output) =
-            prove_epoch_change(&client, &trusted_state, epoch_change_proof).unwrap();
+            prove_epoch_change(&client, &trusted_state, &epoch_change_proof).unwrap();
         println!("Proving took {:?}", start.elapsed());
 
         assert_eq!(
@@ -166,38 +161,17 @@ mod test {
     #[ignore = "This test is too slow for CI"]
     fn test_snark_prove_epoch_change() {
         use super::*;
-        use aptos_lc_core::aptos_test_utils::wrapper::AptosWrapper;
-        use aptos_lc_core::crypto::hash::CryptoHash;
-        use aptos_lc_core::types::trusted_state::TrustedState;
         use sphinx_sdk::ProverClient;
         use std::time::Instant;
 
         setup_logger();
 
-        const NBR_VALIDATORS: usize = 130;
-        const AVERAGE_SIGNERS_NBR: usize = 95;
-
-        let mut aptos_wrapper = AptosWrapper::new(5, NBR_VALIDATORS, AVERAGE_SIGNERS_NBR).unwrap();
-
-        let trusted_state = bcs::to_bytes(aptos_wrapper.trusted_state()).unwrap();
-        let _validator_verifier_hash = match TrustedState::from_bytes(&trusted_state).unwrap() {
-            TrustedState::EpochState { epoch_state, .. } => epoch_state.verifier().hash().to_vec(),
-            _ => panic!("Expected epoch change for current trusted state"),
-        };
-        let trusted_state_version = *aptos_wrapper.current_version();
-
-        aptos_wrapper.generate_traffic().unwrap();
-
-        let state_proof = aptos_wrapper
-            .new_state_proof(trusted_state_version)
-            .unwrap();
-
-        let epoch_change_proof = &bcs::to_bytes(state_proof.epoch_changes()).unwrap();
+        let (trusted_state, epoch_change_proof, _) = setup_assets();
 
         let client = ProverClient::new();
         let (pk, vk) = client.setup(aptos_programs::EPOCH_CHANGE_PROGRAM);
 
-        let stdin = generate_stdin(trusted_state.as_slice(), epoch_change_proof);
+        let stdin = generate_stdin(trusted_state.as_slice(), &epoch_change_proof);
 
         let start = Instant::now();
         println!("Starting generation of prove_epoch_change proof...");
--- aptos/light-client/src/inclusion.rs
+++ aptos/light-client/src/inclusion.rs
@@ -67,6 +67,64 @@ impl ValidatorVerifierAssets {
     }
 }
 
+#[cfg(feature = "aptos")]
+pub fn setup_assets() -> (
+    SparseMerkleProofAssets,
+    TransactionProofAssets,
+    ValidatorVerifierAssets,
+) {
+    use aptos_lc_core::aptos_test_utils::wrapper::AptosWrapper;
+    use aptos_lc_core::types::trusted_state::TrustedState;
+
+    const NBR_VALIDATORS: usize = 130;
+    const AVERAGE_SIGNERS_NBR: usize = 95;
+
+    let mut aptos_wrapper = AptosWrapper::new(500, NBR_VALIDATORS, AVERAGE_SIGNERS_NBR).unwrap();
+    aptos_wrapper.generate_traffic().unwrap();
+
+    let proof_assets = aptos_wrapper.get_latest_proof_account(400).unwrap();
+
+    let sparse_merkle_proof = bcs::to_bytes(proof_assets.state_proof()).unwrap();
+    let key: [u8; 32] = *proof_assets.key().as_ref();
+    let element_hash: [u8; 32] = *proof_assets.state_value_hash().unwrap().as_ref();
+
+    let transaction = bcs::to_bytes(&proof_assets.transaction()).unwrap();
+    let transaction_proof = bcs::to_bytes(&proof_assets.transaction_proof()).unwrap();
+
+    let latest_li = aptos_wrapper.get_latest_li_bytes().unwrap();
+
+    let validator_verifier =
+        match TrustedState::from_bytes(&bcs::to_bytes(&aptos_wrapper.trusted_state()).unwrap())
+            .unwrap()
+        {
+            TrustedState::EpochState { epoch_state, .. } => epoch_state.verifier().clone(),
+            _ => panic!("expected epoch state"),
+        };
+
+    let sparse_merkle_proof_assets = SparseMerkleProofAssets {
+        sparse_merkle_proof,
+        leaf_key: key,
+        leaf_hash: element_hash,
+    };
+
+    let transaction_proof_assets = TransactionProofAssets {
+        transaction,
+        transaction_index: *proof_assets.transaction_version(),
+        transaction_proof,
+        latest_li,
+    };
+
+    let validator_verifier_assets = ValidatorVerifierAssets {
+        validator_verifier: validator_verifier.to_bytes(),
+    };
+
+    (
+        sparse_merkle_proof_assets,
+        transaction_proof_assets,
+        validator_verifier_assets,
+    )
+}
+
 pub fn generate_stdin(
     sparse_merkle_proof_assets: &SparseMerkleProofAssets,
     transaction_proof_assets: &TransactionProofAssets,
@@ -141,69 +199,11 @@ fn prove_inclusion(
 mod test {
     use crate::error::LightClientError;
     use crate::inclusion::{
-        SparseMerkleProofAssets, TransactionProofAssets, ValidatorVerifierAssets,
+        setup_assets, SparseMerkleProofAssets, TransactionProofAssets, ValidatorVerifierAssets,
     };
     use aptos_lc_core::types::validator::ValidatorVerifier;
     use sphinx_sdk::utils::setup_logger;
     use sphinx_sdk::{ProverClient, SphinxStdin};
-
-    fn setup_assets() -> (
-        SparseMerkleProofAssets,
-        TransactionProofAssets,
-        ValidatorVerifierAssets,
-    ) {
-        use aptos_lc_core::aptos_test_utils::wrapper::AptosWrapper;
-        use aptos_lc_core::types::trusted_state::TrustedState;
-
-        const NBR_VALIDATORS: usize = 130;
-        const AVERAGE_SIGNERS_NBR: usize = 95;
-
-        let mut aptos_wrapper =
-            AptosWrapper::new(500, NBR_VALIDATORS, AVERAGE_SIGNERS_NBR).unwrap();
-        aptos_wrapper.generate_traffic().unwrap();
-
-        let proof_assets = aptos_wrapper.get_latest_proof_account(400).unwrap();
-
-        let sparse_merkle_proof = bcs::to_bytes(proof_assets.state_proof()).unwrap();
-        let key: [u8; 32] = *proof_assets.key().as_ref();
-        let element_hash: [u8; 32] = *proof_assets.state_value_hash().unwrap().as_ref();
-
-        let transaction = bcs::to_bytes(&proof_assets.transaction()).unwrap();
-        let transaction_proof = bcs::to_bytes(&proof_assets.transaction_proof()).unwrap();
-
-        let latest_li = aptos_wrapper.get_latest_li_bytes().unwrap();
-
-        let validator_verifier =
-            match TrustedState::from_bytes(&bcs::to_bytes(&aptos_wrapper.trusted_state()).unwrap())
-                .unwrap()
-            {
-                TrustedState::EpochState { epoch_state, .. } => epoch_state.verifier().clone(),
-                _ => panic!("expected epoch state"),
-            };
-
-        let sparse_merkle_proof_assets = SparseMerkleProofAssets {
-            sparse_merkle_proof,
-            leaf_key: key,
-            leaf_hash: element_hash,
-        };
-
-        let transaction_proof_assets = TransactionProofAssets {
-            transaction,
-            transaction_index: *proof_assets.transaction_version(),
-            transaction_proof,
-            latest_li,
-        };
-
-        let validator_verifier_assets = ValidatorVerifierAssets {
-            validator_verifier: validator_verifier.to_bytes(),
-        };
-
-        (
-            sparse_merkle_proof_assets,
-            transaction_proof_assets,
-            validator_verifier_assets,
-        )
-    }
 
     fn execute_inclusion(
         sparse_merkle_proof_assets: &SparseMerkleProofAssets,
