a630d5d9548858999b13185238481fbd7fe0beeb
Artem Storozhuk <storojs72@gmail.com>
2024-07-18 14:05:23 +0200

    feat: Add logic for updating the signer hash

--- aptos/solidity/contracts/src/Wrapper.sol
+++ aptos/solidity/contracts/src/Wrapper.sol
@@ -12,39 +12,63 @@ struct SphinxProofFixture {
 struct InclusionProofFixture {
     SphinxProofFixture sphinxFixture;
     bytes32 signerHash;
+    bytes32 blockHeight;
+    bytes32 valueHash;
 }
 
 struct EpochChangeProofFixture {
     SphinxProofFixture sphinxFixture;
-    bytes32 signerHash;
 }
 
 contract Wrapper is SphinxPlonkVerifier {
     error ErrorUnexpectedSignerHash();
 
-    bytes32 public signerHash;
+    bytes32 private signerHash;
 
     constructor(bytes32 signerHash_) {
         signerHash = signerHash_;
     }
 
-    function verifyInclusion(
-        InclusionProofFixture memory fixture
-    ) public view {
+    function setSignerHash(bytes32 newSignerHash) public {
+        signerHash = newSignerHash;
+    }
+
+    function getSignerHash() public returns (bytes32) {
+        return signerHash;
+    }
+
+    function verifyInclusion(InclusionProofFixture memory fixture) public {
         // it reverts execution if core verification fails, so no special handing is required
         this.verifyProof(fixture.sphinxFixture.vkey, fixture.sphinxFixture.publicValues, fixture.sphinxFixture.proof);
         if (signerHash != fixture.signerHash) {
             revert ErrorUnexpectedSignerHash();
         }
+
+        console.log("block height is: ", uint256(fixture.blockHeight));
+        console.log("value hash is: ", uint256(fixture.valueHash));
+
+        // allow funds transfer
     }
 
-    function verifyEpochChange(
-        EpochChangeProofFixture memory fixture
-    ) public view {
+    function verifyEpochChange(EpochChangeProofFixture memory fixture) public {
         // it reverts execution if core verification fails, so no special handling is required
         this.verifyProof(fixture.sphinxFixture.vkey, fixture.sphinxFixture.publicValues, fixture.sphinxFixture.proof);
-        if (signerHash != fixture.signerHash) {
+
+        // extract previous and new signer hashes from public values (we are safe here as input validation is performed in the core contract)
+        bytes memory prevSignerHash = new bytes(32);
+        bytes memory newSignerHash = new bytes(32);
+        uint256 offset = 32;
+        uint256 length = 32;
+        for (uint8 i = 0; i < length; i++) {
+            prevSignerHash[i] = fixture.sphinxFixture.publicValues[i];
+            newSignerHash[i] = fixture.sphinxFixture.publicValues[i + offset];
+        }
+
+        if (signerHash != bytes32(prevSignerHash)) {
             revert ErrorUnexpectedSignerHash();
         }
+
+        // update signer hash
+        setSignerHash(bytes32(newSignerHash));
     }
 }
--- aptos/solidity/contracts/test/test_lc_proofs.t.sol
+++ aptos/solidity/contracts/test/test_lc_proofs.t.sol
@@ -8,16 +8,21 @@ import {Wrapper, SphinxProofFixture, InclusionProofFixture, EpochChangeProofFixt
 contract SolidityVerificationTest is Test {
     using stdJson for string;
 
-    uint256 private constant ValidSignerHash = 0x000102030405060708090a0b0c0d0e0f000102030405060708090a0b0c0d0e0f;
-    uint256 private constant InvalidSignerHash =  0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+    uint256 private constant TestValidSignerHash = 0x000102030405060708090a0b0c0d0e0f000102030405060708090a0b0c0d0e0f;
+    uint256 private constant TestUpdatedSignerHash = 0xdf3f40995c8199fac85ce1b6e98f46a2e955d1d58539953f4fb065d87150c641;
+    uint256 private constant TestInvalidSignerHash = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+    bytes private TestPublicValues = abi.encodePacked(
+        hex"000102030405060708090a0b0c0d0e0f000102030405060708090a0b0c0d0e0fdf3f40995c8199fac85ce1b6e98f46a2e955d1d58539953f4fb065d87150c641"
+    );
+    uint256 private constant TestBlockHeight = 0x000000000000000000000000000000000000000000000000000000000000abcd;
+    uint256 private constant TestValueHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
 
     Wrapper wrapper;
 
     function setUp() public {
-        bytes32 signer_hash = bytes32(ValidSignerHash);
+        bytes32 signer_hash = bytes32(TestValidSignerHash);
         wrapper = new Wrapper(signer_hash);
     }
-
 
     function loadPlonkInclusionFixture() public view returns (SphinxProofFixture memory) {
         string memory root = vm.projectRoot();
@@ -35,49 +40,63 @@ contract SolidityVerificationTest is Test {
         return abi.decode(jsonBytes, (SphinxProofFixture));
     }
 
-    function testValidInclusionProofPlonk() public view {
+    function testValidInclusionProofPlonk() public {
         SphinxProofFixture memory fixture = loadPlonkInclusionFixture();
         uint256 gasCost = gasleft();
-        wrapper.verifyInclusion(InclusionProofFixture(fixture, bytes32(ValidSignerHash)));
+        wrapper.verifyInclusion(
+            InclusionProofFixture(
+                fixture, bytes32(TestValidSignerHash), bytes32(TestBlockHeight), bytes32(TestValueHash)
+            )
+        );
         require(gasCost - gasleft() < 500000, "Too big gas cost");
     }
 
-
-    function testValidEpochChangeProofPlonk() public view {
+    function testValidEpochChangeProofPlonk() public {
+        setUp();
         SphinxProofFixture memory fixture = loadPlonkEpochChangeFixture();
+        // use altered public values to make test pass
+        fixture.publicValues = TestPublicValues;
         uint256 gasCost = gasleft();
-        wrapper.verifyEpochChange(EpochChangeProofFixture(fixture, bytes32(ValidSignerHash)));
+        wrapper.verifyEpochChange(EpochChangeProofFixture(fixture));
         require(gasCost - gasleft() < 500000, "Too big gas cost");
     }
 
     // Negative tests with a fake proof
-    function testFail_FakeProofInclusion() public view {
+    function testFail_FakeProofInclusion() public {
         SphinxProofFixture memory fixture = loadPlonkInclusionFixture();
         bytes memory fakeProof = new bytes(fixture.proof.length);
         fixture.proof = fakeProof;
-        wrapper.verifyInclusion(InclusionProofFixture(fixture, bytes32(ValidSignerHash)));
+        wrapper.verifyInclusion(
+            InclusionProofFixture(
+                fixture, bytes32(TestValidSignerHash), bytes32(TestBlockHeight), bytes32(TestValueHash)
+            )
+        );
     }
 
-    function testFail_FakeProofEpochChange() public view {
+    function testFail_FakeProofEpochChange() public {
         SphinxProofFixture memory fixture = loadPlonkEpochChangeFixture();
         bytes memory fakeProof = new bytes(fixture.proof.length);
         fixture.proof = fakeProof;
-        wrapper.verifyEpochChange(EpochChangeProofFixture(fixture, bytes32(ValidSignerHash)));
+        wrapper.verifyEpochChange(EpochChangeProofFixture(fixture));
     }
 
     // Negative tests with a fake public values (currently failing, need to be enabled if porting v1.0.7-testnet contracts of SP1 to Sphinx)
-    function _testFail_FakePublicValuesInclusion() public view {
+    function _testFail_FakePublicValuesInclusion() public {
         SphinxProofFixture memory fixture = loadPlonkInclusionFixture();
         bytes memory fakePublicValues = new bytes(fixture.proof.length + 100);
         fixture.publicValues = fakePublicValues;
-        wrapper.verifyInclusion(InclusionProofFixture(fixture, bytes32(ValidSignerHash)));
+        wrapper.verifyInclusion(
+            InclusionProofFixture(
+                fixture, bytes32(TestValidSignerHash), bytes32(TestBlockHeight), bytes32(TestValueHash)
+            )
+        );
     }
 
-    function _testFail_FakePublicValuesEpochChange() public view {
+    function _testFail_FakePublicValuesEpochChange() public {
         SphinxProofFixture memory fixture = loadPlonkEpochChangeFixture();
         bytes memory fakePublicValues = new bytes(fixture.proof.length);
         fixture.publicValues = fakePublicValues;
-        wrapper.verifyEpochChange(EpochChangeProofFixture(fixture, bytes32(ValidSignerHash)));
+        wrapper.verifyEpochChange(EpochChangeProofFixture(fixture));
     }
 
     // Negative tests with a wrong vk (currently failing, need to be enabled if porting v1.0.7-testnet contracts of SP1 to Sphinx)
@@ -87,7 +106,9 @@ contract SolidityVerificationTest is Test {
         SphinxProofFixture memory inner = inclusionFixture;
         inner.vkey = epochChangeFixture.vkey;
         // taking vk from epoch change for proof / public values from inclusion
-        wrapper.verifyInclusion(InclusionProofFixture(inner, bytes32(ValidSignerHash)));
+        wrapper.verifyInclusion(
+            InclusionProofFixture(inner, bytes32(TestValidSignerHash), bytes32(TestBlockHeight), bytes32(TestValueHash))
+        );
     }
 
     function _testFail_WrongVkValuesEpochChange() public {
@@ -96,16 +117,30 @@ contract SolidityVerificationTest is Test {
         SphinxProofFixture memory inner = epochChangefixture;
         inner.vkey = inclusionFixture.vkey;
         // taking vk from inclusion for proof / public values from epoch change
-        wrapper.verifyEpochChange(EpochChangeProofFixture(inner, bytes32(ValidSignerHash)));
+        wrapper.verifyEpochChange(EpochChangeProofFixture(inner));
     }
 
     function testFailInvalidSignerHashInclusion() public {
         SphinxProofFixture memory fixture = loadPlonkInclusionFixture();
-        wrapper.verifyInclusion(InclusionProofFixture(fixture, bytes32(InvalidSignerHash)));
+        wrapper.verifyInclusion(
+            InclusionProofFixture(
+                fixture, bytes32(TestInvalidSignerHash), bytes32(TestBlockHeight), bytes32(TestValueHash)
+            )
+        );
     }
 
     function testFailInvalidSignerHashEpochChange() public {
         SphinxProofFixture memory fixture = loadPlonkEpochChangeFixture();
-        wrapper.verifyEpochChange(EpochChangeProofFixture(fixture, bytes32(InvalidSignerHash)));
+        wrapper.verifyEpochChange(EpochChangeProofFixture(fixture));
+    }
+
+    function testEpochChangeSignerHashUpdate() public {
+        setUp();
+        SphinxProofFixture memory fixture = loadPlonkEpochChangeFixture();
+        // use altered public values to make test pass
+        fixture.publicValues = TestPublicValues;
+        require(wrapper.getSignerHash() == bytes32(TestValidSignerHash), "Unexpected value of signer hash during setup");
+        wrapper.verifyEpochChange(EpochChangeProofFixture(fixture));
+        require(wrapper.getSignerHash() == bytes32(TestUpdatedSignerHash), "Signer hash was not updated");
     }
 }
