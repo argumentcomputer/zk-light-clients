3df2a8a9e7086c1d7126955ad79591feb4402fa0
Samuel Burnham <45365069+samuelburnham@users.noreply.github.com>
2024-08-26 21:41:58 -0400

    ci: Fix cycle regression checker (#188)
    * ci: Fix cycle regression checker
    
    * Test workflow
    
    * Prep for review

--- aptos/core/src/crypto/sig.rs
+++ aptos/core/src/crypto/sig.rs
@@ -120,7 +120,7 @@ impl PublicKey {
     /// A `Vec<u8>` representing the public key.
     pub fn to_bytes(&self) -> Vec<u8> {
         let mut bytes = BytesMut::new();
-        let pub_key_bytes = self.compressed_pubkey.as_ref();
+        let pub_key_bytes: &[u8] = self.compressed_pubkey.as_ref();
 
         bytes.put_slice(&write_leb128(pub_key_bytes.len() as u64));
         bytes.put_slice(pub_key_bytes);
