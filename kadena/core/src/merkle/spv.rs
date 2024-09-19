6c6e85bf44f1090b6b719cb572f6b1d269426611
Thomas Chataigner <tom.chataigner@yahoo.fr>
2024-09-19 12:20:06 +0200

    feat: no need for tag

--- kadena/core/src/merkle/spv.rs
+++ kadena/core/src/merkle/spv.rs
@@ -1,5 +1,5 @@
 use crate::crypto::error::CryptoError;
-use crate::crypto::hash::sha512::{hash_inner, hash_tagged_data};
+use crate::crypto::hash::sha512::{hash_data, hash_inner, hash_tagged_data};
 use crate::crypto::hash::HashValue;
 use crate::merkle::proof::MerkleProof;
 use crate::merkle::subject::Subject;
@@ -30,21 +30,28 @@ impl Spv {
 
     pub fn verify(&self, expected_root: &HashValue) -> Result<bool, ValidationError> {
         // Hash subject for base leaf
-        let mut current_hash = hash_tagged_data(
-            TRANSACTION_TAG,
+        let mut current_hash = hash_data(
             &URL_SAFE_NO_PAD
                 .decode(self.subject().input().as_bytes())
                 .map_err(|err| ValidationError::InvalidBase64 { source: err.into() })?,
         )
         .map_err(|err| ValidationError::HashError { source: err.into() })?;
 
+        let og_hash = current_hash.clone();
+
         // Process each step in the proof
         for step in self.object().steps().iter() {
+            println!("----------------------------------------------------------------------------------");
+            println!("          {}", step.hash());
+            println!("          {}", og_hash);
+            println!("          {}", step.hash() == &og_hash);
+            println!("----------------------------------------------------------------------------------");
+
             // Combine the current hash with the proof hash based on the position (left/right)
             current_hash = match step.side() {
-                0x00 => hash_inner(&step.hash().to_vec(), &current_hash.to_vec())
+                0x00 => hash_inner(step.hash().as_ref(), current_hash.as_ref())
                     .map_err(|err| ValidationError::HashError { source: err.into() })?,
-                0x01 => hash_inner(&current_hash.to_vec(), &step.hash().to_vec())
+                0x01 => hash_inner(current_hash.as_ref(), step.hash().as_ref())
                     .map_err(|err| ValidationError::HashError { source: err.into() })?,
                 _ => {
                     return Err(ValidationError::InvalidPosition {
