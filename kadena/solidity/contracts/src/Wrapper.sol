ff41358e6f57ef9de4f480fa4aefcfaf70d143c4
Artem Storozhuk <storojs72@gmail.com>
2024-09-25 18:13:32 +0300

    feat: Finalize post-processing logic

--- kadena/solidity/contracts/src/Wrapper.sol
+++ kadena/solidity/contracts/src/Wrapper.sol
@@ -14,109 +14,110 @@ contract Wrapper is SphinxPlonkVerifier, Ownable(msg.sender) {
     error ErrorUnexpectedLongestChainFixture();
     error ErrorUnexpectedSpvFixture();
     error ErrorUnexpectedSpvSubjectHash();
+    error ErrorConfirmationWorkIsSmallerThanThreshold();
+    error ErrorUnexpectedLayerHash();
 
-    //uint256[] memory checkpoints;
-    //bytes32 confirmation_work_threshold;
-
-    //function setUp() {
-
-    //}
 
     // state
-    // set_state(state)
-    // get_state()
+    bytes32[] checkpoints;
+    uint256 confirmation_work_threshold;
 
-    function verifyLongestChainProof(SphinxProofFixture memory fixture) public view {
+    constructor(uint256 confirmation_work_threshold_, bytes32[] memory checkpoints_) {
+        // Kadena's Chainweb protocol setting
+        require(checkpoints_.length == 5);
+
+        checkpoints = checkpoints_;
+        confirmation_work_threshold = confirmation_work_threshold_;
+    }
+
+    // FIXME: This function is only for testing purposes!!!
+    function set_head_checkpoint(bytes32 zero_checkpoint) public onlyOwner {
+        checkpoints[0] = zero_checkpoint;
+    }
+
+    // FIXME: This function is only for testing purposes!!!
+    function set_confirmation_work_threshold(uint256 threshold) public onlyOwner {
+        confirmation_work_threshold = threshold;
+    }
+
+    function rotate_checkpoints(bytes32 new_checkpoint) public onlyOwner {
+        // drop hash from [0] position, shift rest of hashes and write new checkpoint to the [4] position
+        checkpoints[0] = checkpoints[1];
+        checkpoints[1] = checkpoints[2];
+        checkpoints[2] = checkpoints[3];
+        checkpoints[3] = checkpoints[4];
+        checkpoints[4] = new_checkpoint;
+    }
+
+    function get_current_checkpoints() public view returns (bytes32[] memory) {
+        return checkpoints;
+    }
+
+    function executeCommonProofProcessingLogic(SphinxProofFixture memory fixture) public {
+        uint256 offset = 0;
+        uint256 i = 0;
+
+        // it reverts execution if core verification fails, so no special handling is required
+        this.verifyProof(fixture.vkey, fixture.publicValues, fixture.proof);
+
+        bytes memory confirmation_work = new bytes(32);
+        for (i = 0; i < 32; i++) {
+            confirmation_work[i] = fixture.publicValues[i];
+        }
+        offset += 32;
+
+        bytes memory first_layer_hash = new bytes(32);
+        for (i = 0; i < 32; i++) {
+            first_layer_hash[i] = fixture.publicValues[i + offset];
+        }
+        offset += 32;
+
+        bytes memory target_layer_hash = new bytes(32);
+        for (i = 0; i < 32; i++) {
+            target_layer_hash[i] = fixture.publicValues[i + offset];
+        }
+        offset += 32;
+
+        if (uint256(bytes32(confirmation_work)) < confirmation_work_threshold) {
+            revert ErrorConfirmationWorkIsSmallerThanThreshold();
+        }
+
+        if (bytes32(first_layer_hash) != checkpoints[0]) {
+            revert ErrorUnexpectedLayerHash();
+        }
+
+        rotate_checkpoints(bytes32(target_layer_hash));
+    }
+
+    function verifyLongestChainProof(SphinxProofFixture memory fixture) public {
         if (fixture.publicValues.length != 32 + 32 + 32) {
             revert ErrorUnexpectedLongestChainFixture();
         }
 
-        // it reverts execution if core verification fails, so no special handling is required
-        this.verifyProof(fixture.vkey, fixture.publicValues, fixture.proof);
+        executeCommonProofProcessingLogic(fixture);
 
-        uint256 offset = 0;
-        uint256 i = 0;
-
-        bytes memory confirmation_work = new bytes(32);
-        for (i = 0; i < 32; i++) {
-            confirmation_work[i] = fixture.publicValues[i];
-        }
-        offset += 32;
-
-        bytes memory first_layer_hash = new bytes(32);
-        for (i = 0; i < 32; i++) {
-            first_layer_hash[i] = fixture.publicValues[i + offset];
-        }
-        offset += 32;
-
-        bytes memory target_layer_hash = new bytes(32);
-        for (i = 0; i < 32; i++) {
-            target_layer_hash[i] = fixture.publicValues[i + offset];
-        }
-        offset += 32;
-
-        console.log("confirmation_work: ");
-        console.logBytes32(bytes32(confirmation_work));
-        console.log("first_layer_hash: ");
-        console.logBytes32(bytes32(first_layer_hash));
-        console.log("target_layer_hash: ");
-        console.logBytes32(bytes32(target_layer_hash));
-
-        // TODO: implement checks
-
-        console.log("All checks passed");
+        console.log("All checks have been passed. State has been updated");
     }
 
-    function verifySpvProof(SphinxProofFixture memory fixture, bytes32 user_submitted_spv_subject_hash) public view {
-        if (fixture.publicValues.length < 32 + 32 + 32 + 8 + 4) {
+    function verifySpvProof(SphinxProofFixture memory fixture, bytes32 user_submitted_spv_subject_hash) public {
+        if (fixture.publicValues.length != 32 + 32 + 32 + 32) {
             revert ErrorUnexpectedSpvFixture();
         }
 
-        // it reverts execution if core verification fails, so no special handling is required
-        this.verifyProof(fixture.vkey, fixture.publicValues, fixture.proof);
+        executeCommonProofProcessingLogic(fixture);
 
-        uint256 offset = 0;
+        // starting from 96 after confirmation_work, first_layer_hash, target_layer_hash (32 + 32 + 32)
+        uint256 offset = 96;
         uint256 i = 0;
-
-        bytes memory confirmation_work = new bytes(32);
-        for (i = 0; i < 32; i++) {
-            confirmation_work[i] = fixture.publicValues[i];
-        }
-        offset += 32;
-
-        bytes memory first_layer_hash = new bytes(32);
-        for (i = 0; i < 32; i++) {
-            first_layer_hash[i] = fixture.publicValues[i + offset];
-        }
-        offset += 32;
-
-        bytes memory target_layer_hash = new bytes(32);
-        for (i = 0; i < 32; i++) {
-            target_layer_hash[i] = fixture.publicValues[i + offset];
-        }
-        offset += 32;
-
         bytes memory subject_hash = new bytes(32);
         for (i = 0; i < 32; i++) {
             subject_hash[i] = fixture.publicValues[i + offset];
         }
-        offset += 32;
-
-        console.log("confirmation_work: ");
-        console.logBytes32(bytes32(confirmation_work));
-        console.log("first_layer_hash: ");
-        console.logBytes32(bytes32(first_layer_hash));
-        console.log("target_layer_hash: ");
-        console.logBytes32(bytes32(target_layer_hash));
-        console.log("subject hash: ");
-        console.logBytes32(bytes32(subject_hash));
-
-        // TODO: implement checks
 
         if (bytes32(subject_hash) != user_submitted_spv_subject_hash) {
             revert ErrorUnexpectedSpvSubjectHash();
         }
 
-        console.log("All checks passed");
+        console.log("All checks have been passed. State has been updated");
     }
 }
--- kadena/solidity/contracts/test/test_lc_proofs.sol
+++ kadena/solidity/contracts/test/test_lc_proofs.sol
@@ -12,7 +12,17 @@ contract SolidityVerificationTest is Test {
     Wrapper wrapper;
 
     function setUp() public {
-        wrapper = new Wrapper();
+        // initial state
+        bytes32[] memory checkpoints = new bytes32[](5);
+        checkpoints[0] = bytes32(0x0000000000000000000000000000000000000000000000000000000000000001);
+        checkpoints[1] = bytes32(0x0000000000000000000000000000000000000000000000000000000000000002);
+        checkpoints[2] = bytes32(0x0000000000000000000000000000000000000000000000000000000000000003);
+        checkpoints[3] = bytes32(0x0000000000000000000000000000000000000000000000000000000000000004);
+        checkpoints[4] = bytes32(0x0000000000000000000000000000000000000000000000000000000000000005);
+
+        uint256 confirmation_work_threshold = 0x596e6483a7e9188e289af6012de83766283712e3ad57bf03dd03000000000001;
+
+        wrapper = new Wrapper(confirmation_work_threshold, checkpoints);
     }
 
     function loadPlonkLongestChainFixture() public view returns (SphinxProofFixture memory) {
@@ -47,8 +57,115 @@ contract SolidityVerificationTest is Test {
         require(gasCost - gasleft() < 300000, "Too big gas cost");
     }
 
-    function testDebug() public {
-        SphinxProofFixture memory fixture = loadPlonkSpvFixture();
-        wrapper.verifySpvProof(fixture, bytes32(0x256348dcc4564a102e6c913bf04b8dc6bc1e8325a8bbc71b49808a49bca7340a));
+    function testSuccessfulLongestChainVerification() public {
+        SphinxProofFixture memory fixture = loadPlonkLongestChainFixture();
+        bytes memory confirmation_work = new bytes(32);
+        uint256 offset = 0;
+        uint256 i = 0;
+        for (i = 0; i < 32; i++) {
+            confirmation_work[i] = fixture.publicValues[i + offset];
+        }
+
+        wrapper.set_confirmation_work_threshold(uint256(bytes32(confirmation_work)));
+
+        bytes memory head_checkpoint = new bytes(32);
+        offset = 32;
+        for (i = 0; i < 32; i++) {
+            head_checkpoint[i] = fixture.publicValues[i + offset];
+        }
+
+        wrapper.set_head_checkpoint(bytes32(head_checkpoint));
+
+        wrapper.verifyLongestChainProof(fixture);
+
+        // check state rotation
+        bytes memory tail_checkpoint = new bytes(32);
+        offset = 64;
+        for (i = 0; i < 32; i++) {
+            tail_checkpoint[i] = fixture.publicValues[i + offset];
+        }
+
+        bytes32[] memory checkpoints = wrapper.get_current_checkpoints();
+        require(checkpoints[0] == 0x0000000000000000000000000000000000000000000000000000000000000002);
+        require(checkpoints[1] == 0x0000000000000000000000000000000000000000000000000000000000000003);
+        require(checkpoints[2] == 0x0000000000000000000000000000000000000000000000000000000000000004);
+        require(checkpoints[3] == 0x0000000000000000000000000000000000000000000000000000000000000005);
+        require(checkpoints[4] == bytes32(tail_checkpoint));
     }
+
+    function testSuccessfulSpvVerification() public {
+        SphinxProofFixture memory fixture = loadPlonkSpvFixture();
+        bytes memory confirmation_work = new bytes(32);
+        uint256 offset = 0;
+        uint256 i = 0;
+        for (i = 0; i < 32; i++) {
+            confirmation_work[i] = fixture.publicValues[i + offset];
+        }
+
+        wrapper.set_confirmation_work_threshold(uint256(bytes32(confirmation_work)));
+
+        bytes memory head_checkpoint = new bytes(32);
+        offset = 32;
+        for (i = 0; i < 32; i++) {
+            head_checkpoint[i] = fixture.publicValues[i + offset];
+        }
+
+        wrapper.set_head_checkpoint(bytes32(head_checkpoint));
+
+        offset = 96;
+        bytes memory subject_hash = new bytes(32);
+        for (i = 0; i < 32; i++) {
+            subject_hash[i] = fixture.publicValues[i + offset];
+        }
+        wrapper.verifySpvProof(fixture, bytes32(subject_hash));
+
+        // check state rotation
+        bytes memory tail_checkpoint = new bytes(32);
+        offset = 64;
+        for (i = 0; i < 32; i++) {
+            tail_checkpoint[i] = fixture.publicValues[i + offset];
+        }
+
+        bytes32[] memory checkpoints = wrapper.get_current_checkpoints();
+        require(checkpoints[0] == 0x0000000000000000000000000000000000000000000000000000000000000002);
+        require(checkpoints[1] == 0x0000000000000000000000000000000000000000000000000000000000000003);
+        require(checkpoints[2] == 0x0000000000000000000000000000000000000000000000000000000000000004);
+        require(checkpoints[3] == 0x0000000000000000000000000000000000000000000000000000000000000005);
+        require(checkpoints[4] == bytes32(tail_checkpoint));
+    }
+
+    function testFailSmallerConfirmationWorkThreshold1() public {
+        SphinxProofFixture memory fixture = loadPlonkSpvFixture();
+        bytes memory head_checkpoint = new bytes(32);
+        uint256 offset = 32;
+        uint256 i = 0;
+        for (i = 0; i < 32; i++) {
+            head_checkpoint[i] = fixture.publicValues[i + offset];
+        }
+
+        wrapper.set_head_checkpoint(bytes32(head_checkpoint));
+
+        offset = 96;
+        i = 0;
+        bytes memory subject_hash = new bytes(32);
+        for (i = 0; i < 32; i++) {
+            subject_hash[i] = fixture.publicValues[i + offset];
+        }
+        wrapper.verifySpvProof(fixture, bytes32(subject_hash));
+    }
+
+    function testFailSmallerConfirmationWorkThreshold2() public {
+        SphinxProofFixture memory fixture = loadPlonkLongestChainFixture();
+        bytes memory head_checkpoint = new bytes(32);
+        uint256 offset = 32;
+        uint256 i = 0;
+        for (i = 0; i < 32; i++) {
+            head_checkpoint[i] = fixture.publicValues[i + offset];
+        }
+
+        wrapper.set_head_checkpoint(bytes32(head_checkpoint));
+
+        wrapper.verifyLongestChainProof(fixture);
+    }
+
 }
